130028c6f54cc6eb3f983ffcc71d62284c22154d
“pankajnaik882” <pankaj080519999@gmail.com>
2023-02-24 18:58:01 +0530

    Add : Added Node.cs File For Enqueue Function

--- Node.cs
+++ Node.cs
@@ -11,9 +11,7 @@ namespace StackAndQueue
         public int data;
         public Node next;
         public Node(int data)
-        {  
-
-
+        {
             this.data = data;
         }
     }
