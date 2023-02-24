d1efa1bea0766082e085804c9bc58f48a50d18ba
“pankajnaik882” <pankaj080519999@gmail.com>
2023-02-24 18:19:41 +0530

    Add : Added Program.cs File

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+
+namespace StackAndQueue
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            LinkedList list= new LinkedList();
+            Queue q1 = new Queue();
+            q1.Enqueue(56);
+            q1.Enqueue(30);
+            q1.Enqueue(70);
+
+            foreach (int i in q1)
+            {
+                list.Add(i);
+            }
+            Console.WriteLine("\nTop of the stack : {0}",q1.Peek());
+            Console.WriteLine("************************");
+        }
+
+    }
+}
