074f57eb94db2f578b7c495ad01e79dead82b230
“pankajnaik882” <pankaj080519999@gmail.com>
2023-02-24 19:01:38 +0530

    Add : Added Program.cs File

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace StackAndQueue
     {
         public static void Main(string[] args)
         {
-            LinkedList list= new LinkedList();
+            LinkedList list = new LinkedList();
             Queue q1 = new Queue();
             q1.Enqueue(56);
             q1.Enqueue(30);
@@ -17,8 +17,33 @@ namespace StackAndQueue
             {
                 list.Add(i);
             }
-            Console.WriteLine("\nTop of the stack : {0}",q1.Peek());
+            Console.WriteLine("\nTop of the stack : {0}", q1.Peek());
             Console.WriteLine("************************");
+
+            q1.Dequeue();
+            foreach (int i in q1)
+            {
+                list.Add(i);
+            }
+            Console.WriteLine("\nTop of the stack : {0}", q1.Peek());
+            Console.WriteLine("************************");
+
+            q1.Dequeue();
+            foreach (int i in q1)
+            {
+                list.Add(i);
+            }
+            Console.WriteLine("\nTop of the stack : {0}", q1.Peek());
+            Console.WriteLine("************************");
+
+            q1.Dequeue();
+            foreach (int i in q1)
+            {
+                list.Add(i);
+            }
+            Console.WriteLine("\nTop of the stack : {0}", q1.Peek());
+            Console.WriteLine("************************");
+            Console.WriteLine("Queue was Empty");
         }
 
     }
